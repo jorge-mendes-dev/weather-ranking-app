fe2ac58534a98aa0ef44771a066814c501ae7e6e
Jorge Mendes <br0jmns@festo.net>
2026-04-21 18:05:57 -0300

    add favicon

---
+++ apps/web/src/app/head.tsx
@@ -0,0 +1,9 @@
+// This file customizes the <head> for the Next.js App Router
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      {/* You can add more meta tags here if needed */}
+    </>
+  );
+}
